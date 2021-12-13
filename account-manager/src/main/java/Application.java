4c911855b05579ffbba8aa54b4800a92c7857773
PabloValenzuelaa <valenzuelapablo1998@gmail.com>
2021-12-12 23:07:52 -0300

    avance account manager

--- account-manager/src/main/java/Application.java
+++ account-manager/src/main/java/Application.java
@@ -21,25 +21,34 @@ public class Application {
 
     public static void consumeMessages(String topic, Consumer<String, Transaction> kafkaConsumer) {
         kafkaConsumer.subscribe(Collections.singletonList(topic));
-
-        while (true) {
-            /**
-             * Complete el codigo aca en caso que sea necesario
-             * Verifique si hay nuevas transacciones a leer desde Kafka.
-             * Apruebe las transacciones entrantes
-             */
-
-            kafkaConsumer.commitAsync();
+        try {
+	        while (true) {
+	            /**
+	             * Complete el codigo aca en caso que sea necesario
+	             * Verifique si hay nuevas transacciones a leer desde Kafka.
+	             * Apruebe las transacciones entrantes
+	             */
+	        	ConsumerRecords<String, String> record = kafkaConsumer.poll(100);
+	        	for(ConsumerRecord <String, String> redord : records) {
+	        		System.out.println(record.toString());
+	        	}
+	        	
+	            kafkaConsumer.commitAsync();
+	        }
+        } catch(Exeption e) {
+        	System.out.println(e);
+        } finally {
+        	kafkaConsumer.close();
         }
     }
 
     public static Consumer<String, Transaction> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
         Properties properties = new Properties();
 
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, /** Complete el codigo aca en caso que sea necesario **/);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, /** Complete el codigo aca en caso que sea necesario **/);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, /** Complete el codigo aca en caso que sea necesario **/);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, /** Complete el codigo aca en caso que sea necesario **/);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         return new KafkaConsumer<>(properties);
